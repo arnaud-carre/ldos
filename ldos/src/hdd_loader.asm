49dfbc36ff7172f190d50d60fc1db6b75a4468a4
Arnaud Carré <arnaud.carre@gmail.com>
2023-03-26 19:03:05 +0200

    simplify hdd_loader

--- ldos/src/hdd_loader.asm
+++ ldos/src/hdd_loader.asm
@@ -10,28 +10,25 @@
 ;---------------------------------------------------------
 
 
-_LVOAvailMem		=	-216	
-_LVOAllocMem		=	-198	
+		macro	ADF_DISK_NAME
+			dc.b	"cycle-op.adf"
+		endm
+
 _LVOOpenLib			=	-552
 _LVOCloseLib		=	-414
-
 _LVOOpenFile		=	-30
 _LVOCloseFile		=	-36
 _LVOReadFile		=	-42
 _LVOWriteFile		=	-48
 
-MEMF_CHIP   	=	(1<<1)	; Chip memory
-MEMF_FAST		=	(1<<2)	; Fast memory
-MEMF_LARGEST	=	(1<<17)	; AvailMem: return the largest chunk size
-
 DISK1_SIZE			=	880*1024
-DISK2_SIZE			=	880*1024
+
 
 
 		include "kernelPrivate.inc"
 
 		
-	;	code
+		code
 	
 start:
 
@@ -64,51 +61,40 @@ start:
 			move.l	d0,conHandle
 			beq		exitProg
 
-		; alloc and load first ADF disk
-			lea	floppy1Name(pc),a0
+		; Load ADF disk
+			lea		floppy1Name(pc),a0
 			move.l	#DISK1_SIZE,d0
-			bsr	loadFile
-			move.l	a0,m_hddBuffer1(a7)
+			lea		diskBuffer,a1
+			move.l	a1,m_hddBuffer1(a7)
+			bsr		loadFile
 
-		; alloc and load second ADF disk
 			clr.l	m_hddBuffer2(a7)
-;			lea	floppy2Name(pc),a0
-;			move.l	#DISK2_SIZE,d0
-;			bsr	loadFile
-;			move.l	a0,m_hddBuffer2(a7)
 
-		; finally alloc max block of FAST and CHIP mem
-			move.l	$4.w,a6
-			move.l	#(512*1024),d0
-			move.l	#MEMF_CHIP,d1
-			jsr		_LVOAllocMem(a6)
-			tst.l	d0
-			beq		mallocError
-			move.l	d0,m_chipStart(a7)
-
-			move.l	#(512*1024),d0
-			moveq	#0,d1							; MEMF_ANY
-			jsr		_LVOAllocMem(a6)
-			tst.l	d0
-			beq		mallocError
-			move.l	d0,m_fakeStart(a7)
+		; set the CHIP & ANY buffers addr
+			move.l	#chipBuffer,m_chipStart(a7)
+			move.l	#anyBuffer,m_fakeStart(a7)
 
 		; search the NOP to jump in the code
 			move.l	m_hddBuffer1(a7),a1
 			movea.l	a1,a2
+			lea		512(a2),a3
 .search:	cmpi.w	#$4e71,(a2)+
+			beq.s	.found
+			cmpa.l	a2,a3
 			bne.s	.search
+			bra		exitProg		; ERROR: NOP not found in bootsector, probably means it's not LDOS ADF file
 
-			move.l	m_chipStart(a7),a0
+.found:		move.l	m_chipStart(a7),a0
 			add.l	#(512-64)*1024,a0
 			jmp		(a2)				; jump in the bootsector code
 
 		
 ; a0: file name
+; a1: buffer
 ; d0: bytes to load
-; returns: a0: load buffer ad (0 if ERROR)	
 loadFile:
 			move.l	d0,fsize
+			move.l	a1,pBuffer
 
 			move.l	a0,d1
 			tst.b	isOS2
@@ -118,77 +104,52 @@ loadFile:
 			move.l	dosHandle,a6
 			jsr		_LVOOpenFile(a6)
 			tst.l	d0
-			beq	fileNotFound
+			beq		readError
 			move.l	d0,fileH
 
-			move.l	$4.w,a6
-			move.l	fsize,d0			; size to read
-			moveq	#0,d1
-			jsr		_LVOAllocMem(a6)
-			tst.l	d0
-			beq		mallocError
-			move.l	d0,pBuffer
-
-			move.l	fileH,d1			; file handle
-			move.l	pBuffer,d2			; Buffer Ad.
-			move.l	fsize,d3			; read size
-			move.l	dosHandle,a6
+			move.l	fileH(pc),d1			; file handle
+			move.l	pBuffer(pc),d2			; Buffer Ad.
+			move.l	fsize(pc),d3			; read size
+			move.l	dosHandle(pc),a6
 			jsr		_LVOReadFile(a6)
 			tst.l	d0
 			beq		readError
 
-			move.l	fileH,d1
-			move.l	dosHandle,a6
+			move.l	fileH(pc),d1
+			move.l	dosHandle(pc),a6
 			jsr		_LVOCloseFile(a6)
 
-			move.l	pBuffer,a0
 			rts
 
 			
 exitProg:
 
 		; close console
-			move.l	conHandle,d1
+			move.l	conHandle(pc),d1
 			beq.s	.nocon
-			move.l	dosHandle,a6
+			move.l	dosHandle(pc),a6
 			jsr		_LVOCloseFile(a6)
 
 		; close DOS library
-.nocon:		move.l	dosHandle,a1
+.nocon:		move.l	dosHandle(pc),a1
 			move.l	$4.w,a6
 			jsr		_LVOCloseLib(a6)
 
 		; exit
-			move.l	pOriginalStack,a7
+			move.l	pOriginalStack(pc),a7
 			moveq	#0,d0
 			rts
 			
-			
-waitKey:	lea		txtPressReturn,a0
+waitKey:	lea		txtPressReturn(pc),a0
 			bsr		gemdos9
 			bsr		gemdos7
 			rts
-			
-mallocError:
-			lea		txtMallocError,a0
-			bsr		gemdos9
-			bsr		waitKey
-			bra		exitProg
-
-
-			
-fileNotFound:
-			lea		txtFNFError,a0
-			bsr		gemdos9
-			bsr		waitKey
-			bra		exitProg
 
 readError:
-			lea		txtReadError,a0
+			lea		txtReadError(pc),a0
 			bsr		gemdos9
 			bsr		waitKey
 			bra		exitProg
-
 
 ; Gemdos9 is "print" on ATARI :)
 gemdos9:	move.l	a0,d2			; String AD
@@ -209,30 +170,32 @@ gemdos7:	move.l	conHandle,d1
 			jsr		_LVOReadFile(a6)
 			rts
 			
-	;data
-
 conHandle:		dc.l	0
 dosHandle:		dc.l	0
-
 dosLibName:		dc.b	'dos.library',0
 consoleName:	dc.b	'CON:0/224/480/24/LDOS HDD Loader...',0
-floppy1Name:	dc.b	'PROGDIR:ldos_demo.adf',0
-;floppy2Name:	dc.b	'PROGDIR:ldos_demo_d2.adf',0
+floppy1Name:	dc.b	"PROGDIR:"
+				ADF_DISK_NAME
+				dc.b	0
+txtReadError:	dc.b	'Unable to load the ADF file',10,0
 txtPressReturn:	dc.b	10,'Press RETURN key',0
-txtMallocError:	dc.b	'Floppy version requires 1MiB RAM',10
-				dc.b	'HDD version requieres 3MiB RAM',10,0
-txtFNFError:	dc.b	'Unable to find ADF file to load',10,0
-txtReadError:	dc.b	'Error reading the ADF file',10,0
 isOS2:			dc.b	0
-
-
 				even
-			
-	;bss
 
 pOriginalStack:	ds.l	1	
-fsize:		ds.l	1			
-fileH:		ds.l	1
-pBuffer:	ds.l	1
-buffer		ds.b	8
+fsize:			ds.l	1			
+fileH:			ds.l	1
+pBuffer:		ds.l	1
+buffer			ds.b	8
 
+	bss disk_buffer
+
+diskBuffer:		ds.b	DISK1_SIZE
+
+	bss any_ram
+
+anyBuffer:		ds.b	512*1024
+
+	bss_c chip_ram
+
+chipBuffer:		ds.b	512*1024
