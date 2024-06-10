a662bc865211e5375fa4d3af7315dc88a39cea45
Arnaud <arnaud.carre@gmail.com>
2024-06-10 14:40:08 +0200

    added LDOS_FREE_ALL_BINARY_BLOB function

--- ldos/src/kernel.asm
+++ ldos/src/kernel.asm
@@ -269,6 +269,7 @@ kernelLibrary:
 			bra.w	setNextFileId
 			bra.w	getBlackboardAddr
 			bra.w	preloadFromLz4Memory
+			bra.w	freeAnyBinaryBlob
 			bra.w	wipePreviousFx
 			
 			
@@ -721,6 +722,12 @@ loadBinaryBlob:
 			clr.l	m_ad(a6)			; this is not a standard exe pre_loaded file
 
 			rts
+
+freeAnyBinaryBlob:
+			move.b	#MEMLABEL_PRECACHED_FX,d0
+			bsr		freeMemLabel
+			rts
+
 
 ;-----------------------------------------------------------------		
 ; input:
